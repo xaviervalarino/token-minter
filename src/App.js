4e3c6441ec75464c86697e7cc6f832db704f8c79
Xavier Valarino <xvalarino@pinterest.com>
2021-09-08 14:08:25 -0700

    Create routes object and pass it to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { Box } from 'gestalt';
 import './App.css';
@@ -8,14 +8,38 @@ import ReceiveAccessCode from './pages/ReceiveAccessCode';
 import ExchangeForAccessToken from './pages/ExchangeForAccessToken';
 
 export default function App(props) {
+  const routes = [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path:'/start-oauth-flow',
+      component: StartOAuthFlow
+    },
+    {
+      path:'/receive-access-code',
+      component: ReceiveAccessCode
+    },
+    {
+      path:'/exchange-for-access-token',
+      component: ExchangeForAccessToken
+    }
+  ];
+  const builtRoutes = routes.map( ({ path, component: Component }, i) => {
+    const exact = !i ? true: false;
+    return <Route
+      key={i}
+      exact={exact}
+      path={path}
+      render={ (props) => <Component routes={routes} {...props} /> }
+    />
+  });
   return (
     <Box padding={10}>
         <HashRouter>
           <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/start-oauth-flow' component={StartOAuthFlow} />
-              <Route path='/receive-access-code' component={ReceiveAccessCode} />
-              <Route path='/exchange-for-access-token' component={ExchangeForAccessToken} />
+            { builtRoutes }
           </Switch>
         </HashRouter>
     </Box>
