67552762bcf2a398af41cb2c5e68bb0cfab5158a
Xavier Valarino <xvalarino@pinterest.com>
2021-09-13 13:49:19 -0700

    Allow moving the window with a mouse

--- renderer/components/PageLayout.js
+++ renderer/components/PageLayout.js
@@ -4,21 +4,30 @@ import { DataProvider } from './DataContext'
 
 export default function PageLayout({ page: Page, routes }) {
   return (
-    <Box
-      position='absolute'
-      top bottom left right
-      display='flex'
-      direction='column'
-      overflow='hidden'
-    >
-      <Box flex='grow' overflow='auto'>
-        <Box padding={10}>
-          <DataProvider>
-            <Page />
-          </DataProvider>
+    <>
+      {/* Target area for moving the window around with a mouse */}
+      <Box
+        height={40}
+        dangerouslySetInlineStyle={{
+          __style: { WebkitAppRegion: 'drag' }
+        }}
+      />
+      <Box
+        position='absolute'
+        top bottom left right
+        display='flex'
+        direction='column'
+        overflow='hidden'
+      >
+        <Box flex='grow' overflow='auto'>
+          <Box padding={10}>
+            <DataProvider>
+              <Page />
+            </DataProvider>
+          </Box>
         </Box>
+        <BottomButtonNav routes={routes} />
       </Box>
-      <BottomButtonNav routes={routes} />
-    </Box>
+    </>
   )
 }
