dda8c559f71db885d7d531f877c7e4d5fafe0e7b
Xavier Valarino <xavier@valarino.com>
2021-12-16 10:58:57 -0800

    Fix `no-unstable-nested-components` lint error

--- renderer/components/DataDisplay.js
+++ renderer/components/DataDisplay.js
@@ -12,29 +12,30 @@ import {
 } from 'gestalt';
 import CodeBlock from './CodeBlock';
 
+function DataDisplaySheet({ data, onDismiss }) {
+  const footer = ({ onDismissStart }) => (
+    <Flex justifyContent="end" gap={0}>
+      <Button color="red" size="lg" text="Got it" onClick={onDismissStart} />
+    </Flex>
+  );
+  return (
+    <Sheet
+      accessibilityDismissButtonLabel=""
+      accessibilitySheetLabel="Current app data"
+      size="sm"
+      onDismiss={onDismiss}
+      heading="Current app data"
+      footer={footer}
+    >
+      <Box marginStart={-8} marginEnd={-8}>
+        <CodeBlock>{JSON.stringify(data, null, '\r  ')}</CodeBlock>
+      </Box>
+    </Sheet>
+  );
+}
+
 export default function DataDisplay({ data }) {
   const [showSheet, setShowSheet] = useState(false);
-
-  function DataDisplaySheet({ onDismiss }) {
-    return (
-      <Sheet
-        accessibilityDismissButtonLabel=""
-        accessibilitySheetLabel="Current app data"
-        size="sm"
-        onDismiss={onDismiss}
-        heading="Current app data"
-        footer={({ onDismissStart }) => (
-          <Flex justifyContent="end" gap={0}>
-            <Button color="red" size="lg" text="Got it" onClick={onDismissStart} />
-          </Flex>
-        )}
-      >
-        <Box marginStart={-8} marginEnd={-8}>
-          <CodeBlock>{JSON.stringify(data, null, '\r  ')}</CodeBlock>
-        </Box>
-      </Sheet>
-    );
-  }
 
   const HEADER_ZINDEX = new FixedZIndex(10);
   const zIndex = new CompositeZIndex([HEADER_ZINDEX]);
